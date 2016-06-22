2b86c7813db93f73bd1461f775dcb007b86c4f34
neldevfull <nfrugeri@gmail.com>
2016-06-22 11:40:21 +0000

    Testing API

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from "express";
-const PORT = 3000;
 
-const app = express();
+
+const PORT = 3000;
+let   app  = express();
 
 app.get("/", (req, res) => res.json({status: "DontStop API"}));
 app.listen(PORT, () => console.log(`DontStop API - port ${PORT}`));
