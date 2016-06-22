2350faadca51d671aeb5c16429c24983b4f119ea
neldevfull <nfrugeri@gmail.com>
2016-06-22 12:03:28 +0000

    Created endpoint tasks

--- app.js
+++ app.js
@@ -4,5 +4,20 @@ import express from "express";
 const PORT = 3000;
 let   app  = express();
 
+// Format JSON
+app.set("json spaces", 4);
+
+// Meets at the root
 app.get("/", (req, res) => res.json({status: "DontStop API"}));
+
+app.get("/tasks", (req, res) => {
+    res.json({
+        tasks: [
+            {title: "Go shopping!"},
+            {title: "Clean the house"}
+        ]
+    });
+});
+
+// Run server
 app.listen(PORT, () => console.log(`DontStop API - port ${PORT}`));
